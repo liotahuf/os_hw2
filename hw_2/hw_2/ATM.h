7dd9981d751a34b21d7745d988bd0fffccc15853
liotahuf <liorahuf@gmail.com>
2019-12-10 10:51:30 +0200

    atam.h started

---
+++ hw_2/hw_2/ATM.h
@@ -0,0 +1,17 @@
+#ifndef _ATM_H
+#define _ATM_H
+#include <unistd.h> 
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h>
+
+
+
+
+
+#endif
